863ff07190b028b05af251640dc00b979ed3d00f
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-10-12 03:21:00 +0300

    fv

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; 
+const API_BASE_URL = 'https://incredible-beignet-708e03.netlify.app'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
