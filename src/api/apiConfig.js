a96d2fd687757c145371dee154fe05d3772cf0e2
Kirubel Wondwoson <kirubelwondwoson635@gmail.com>
2024-10-15 04:38:26 +0300

    api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://qemer-tech-registration-server.onrender.com'; 
+const API_BASE_URL = 'http://localhost:5000'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
